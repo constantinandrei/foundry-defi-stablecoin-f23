548f585a6c32a74523002299765092326a3402f0
constantinandrei <andrei.todirascu@gmail.com>
2024-01-16 07:37:02 +0100

    helper config for sepolia and anvile localt network and mocks

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -10,10 +12,19 @@ contract HelperConfig is Script {
         uint256 deployerKey;
     }
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
+    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
-        activeNetworkConfig = getSepoliaConfig();
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
     }
 
     function getSepoliaConfig() private view returns (NetworkConfig memory) {
@@ -28,9 +39,27 @@ contract HelperConfig is Script {
         return config;
     }
 
-    function getOrCreateAnvilEthConfig() private view returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() private returns (NetworkConfig memory) {
         if (activeNetworkConfig.priceFeeds[0] != address(0)) {
             return activeNetworkConfig;
         }
+
+        vm.startBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+        ERC20Mock wethMock = new ERC20Mock();
+        MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
+        ERC20Mock wbtcMock = new ERC20Mock();
+        vm.stopBroadcast();
+
+        NetworkConfig memory config;
+        config.collateralTokens = new address[](2);
+        config.collateralTokens[0] = address(wethMock);
+        config.collateralTokens[1] = address(wbtcMock);
+        config.priceFeeds = new address[](2);
+        config.priceFeeds[0] = address(ethUsdPriceFeed);
+        config.priceFeeds[1] = address(btcUsdPriceFeed);
+        config.deployerKey = DEFAULT_ANVIL_KEY;
+
+        return config;
     }
 }
