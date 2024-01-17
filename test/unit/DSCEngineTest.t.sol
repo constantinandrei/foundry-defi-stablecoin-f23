841a2f8cf6c446ad26b75c00e11cb913447ee2bc
constantinandrei <andrei.todirascu@gmail.com>
2024-01-17 08:39:03 +0100

    adds one test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,10 +16,15 @@ contract DSCEngineTest is Test {
     address ethUsdPriceFeed;
     address weth;
 
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dscEngine, helperConfig) = deployer.run();
         (ethUsdPriceFeed,, weth,,) = helperConfig.activeNetworkConfig();
+        ERC20Mock(weth).mint(address(this), STARTING_ERC20_BALANCE);
     }
 
     function testGetUsdValue() public {
@@ -26,5 +32,13 @@ contract DSCEngineTest is Test {
         uint256 expectedUsdValue = 30000e18;
         uint256 usdValue = dscEngine.getUsdValue(weth, ethAmount);
         assertEq(usdValue, expectedUsdValue);
+    }
+
+    function testIfCollateralIsZero() public {
+        vm.startPrank(USER); 
+        ERC20Mock(weth).approve(address(dscEngine), AMOUNT_COLLATERAL);
+        vm.expectRevert(DSCEngine.DSCEngine__MustBeMostThenZero.selector);
+        dscEngine.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
